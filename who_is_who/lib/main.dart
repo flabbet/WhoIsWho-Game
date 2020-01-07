5dabeea225165c66b2222732153e5f73210cb65d
Frytek <flubbet@gmail.com>
2020-01-07 18:37:13 +0100

    Added flipping cards

--- who_is_who/lib/main.dart
+++ who_is_who/lib/main.dart
@@ -1,7 +1,10 @@
 import 'dart:async';
 import 'dart:convert';
 
+import 'package:flip_card/flip_card.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 import 'JSONLoader.dart';
 import 'Person.dart';
@@ -40,6 +43,8 @@ class _GameHomePageState extends State<GameHomePage> {
   double timerValue = 0;
   var stopwatch = Stopwatch();
   double _elapsedTime = 0;
+  bool tappedAgain = false;
+  GlobalKey<FlipCardState> cardKey = GlobalKey<FlipCardState>();
 
   @override
   void initState() {
@@ -72,6 +77,8 @@ class _GameHomePageState extends State<GameHomePage> {
     });
 
     if (_elapsedTime * 10 >= SecondsPerQuestion) {
+      stopwatch.reset();
+      _elapsedTime = 0;
       _switchQuestion(false);
     }
     if (stopwatch.isRunning) {
@@ -80,8 +87,18 @@ class _GameHomePageState extends State<GameHomePage> {
   }
 
   void _switchQuestion(bool wasGoodAnswer) {
-    if (wasGoodAnswer) {
+    if (tappedAgain) {
+      cardKey.currentState.toggleCard();
+      tappedAgain = false;
+      stopwatch.start();
+      updateStopwatch();
+    } else if (wasGoodAnswer) {
       goodAnswers++;
+    } else {
+      tappedAgain = true;
+      cardKey.currentState.toggleCard();
+      stopwatch.stop();
+      return;
     }
 
     if (currentPersonIndex == _questionsCount - 1) {
@@ -98,9 +115,9 @@ class _GameHomePageState extends State<GameHomePage> {
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
                   Text("Your score was"),
-                  Text(goodAnswers.toString() +
-                      "/" +
-                      _questionsCount.toString(), style: TextStyle(fontSize: 30),),
+                  Text(
+                      goodAnswers.toString() + "/" + _questionsCount.toString(),
+                      style: TextStyle(fontSize: 30)),
                   FlatButton(
                       child: Text("Try again?"),
                       color: Colors.blue,
@@ -138,51 +155,87 @@ class _GameHomePageState extends State<GameHomePage> {
                     "/" +
                     _questionsCount.toString(),
                 style: TextStyle(fontSize: 18)),
-            Card(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15)),
-              color: Colors.white,
-              elevation: 10,
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                children: <Widget>[
-                  ClipRRect(
-                      borderRadius: BorderRadius.circular(8.0),
-                      child: people.length > 0
-                          ? Image.asset(
-                              "assets/images/" +
-                                  people[currentPersonIndex].fileName,
-                              width: 300,
-                              height: 450,
-                              fit: BoxFit.cover)
-                          : null),
-                  FlatButton(
-                    child: const Text("Do you know who is that?"),
-                    onPressed: () {},
+            FlipCard(
+              key: cardKey,
+              direction: FlipDirection.HORIZONTAL,
+              flipOnTouch: false,
+              front: Container(
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  color: Colors.white,
+                  elevation: 10,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    children: <Widget>[
+                      ClipRRect(
+                          borderRadius: BorderRadius.circular(8.0),
+                          child: people.length > 0
+                              ? Image.asset(
+                                  "assets/images/" +
+                                      people[currentPersonIndex].fileName,
+                                  width: 300,
+                                  height: 450,
+                                  fit: BoxFit.cover)
+                              : null),
+                      FlatButton(
+                        child: const Text("Do you know who is that?"),
+                        onPressed: () {},
+                      ),
+                    ],
                   ),
-                ],
+                ),
+              ),
+              back: Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15)),
+                color: Color(0xFFd55563),
+                elevation: 10,
+                child: Container(
+                  width: 300,
+                  height: 450,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Text(tappedAgain ? people[currentPersonIndex].name : "",
+                          style: TextStyle(fontSize: 34, color: Colors.white)),
+                    ],
+                  ),
+                ),
               ),
             ),
             ButtonBar(
               alignment: MainAxisAlignment.center,
-              children: <Widget>[
-                FlatButton(
-                  color: Colors.red,
-                  textColor: Colors.white,
-                  child: Text("No"),
-                  onPressed: () {
-                    _switchQuestion(false);
-                  },
-                ),
-                FlatButton(
-                  color: Colors.green,
-                  textColor: Colors.white,
-                  child: Text("Yes"),
-                  onPressed: () {
-                    _switchQuestion(true);
-                  },
-                )
-              ],
+              children: tappedAgain
+                  ? <Widget>[
+                      FlatButton(
+                        onPressed: () {
+                          _switchQuestion(false);
+                        },
+                        child: Text("Next question", style: TextStyle(fontSize: 18)),
+                        color: Colors.blue,
+                        textColor: Colors.white,
+                      )
+                    ]
+                  : <Widget>[
+                      FlatButton(
+                        color: Colors.red,
+                        textColor: Colors.white,
+                        child: Text("No"),
+                        onPressed: () {
+                          _switchQuestion(false);
+                        },
+                      ),
+                      FlatButton(
+                        color: Colors.green,
+                        textColor: Colors.white,
+                        child: Text("Yes"),
+                        onPressed: () {
+                          _switchQuestion(true);
+                        },
+                      )
+                    ],
             )
           ],
         ),
